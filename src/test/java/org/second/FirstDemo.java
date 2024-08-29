61c3bb6cd4ec185f6aa5ecbf4dcfe44fc81207f3
LalithaDevi96 <lalithadevi96@gmail.com>
2024-08-29 13:19:19 +0530

    commit after cloning

---
+++ src/test/java/org/second/FirstDemo.java
@@ -0,0 +1,12 @@
+package org.second;
+
+public class FirstDemo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello apple");
+		System.out.println("hello apple");
+		
+	}
+
+}
